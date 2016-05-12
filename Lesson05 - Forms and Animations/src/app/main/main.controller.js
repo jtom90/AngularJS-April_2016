429069147debef868d4fd56b5b905821309ce97c
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2016-05-11 20:37:44 -0400

    forms part2

--- Lesson05 - Forms and Animations/src/app/main/main.controller.js
+++ Lesson05 - Forms and Animations/src/app/main/main.controller.js
@@ -11,6 +11,33 @@
     self.greeting = "Hello World"
 
     self.phoneExp = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/;
+
+    self.submitTheForm = function() {
+    	if (self.myForm.$valid) {
+    		// submit the form
+    	} else {
+    		// alert phone is not submitted
+    	}
+    };
+
+    // self.states = ['California', 'Florida', 'New York'];
+
+    // self.states = [{
+    // 	name: 'California',
+    // 	abbr: 'CA'
+    // }, {
+    // 	name: 'Florida',
+    // 	abbr: 'FL'
+    // }, {
+    // 	name: 'New York',
+    // 	abbr: 'NY'
+    // }];
+
+    self.states = {
+    	NY: 'New York',
+    	FL: 'Florida',
+    	CA: 'California'
+    }
   }
 
 })();
